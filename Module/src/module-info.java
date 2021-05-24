78faa0292e9502d8ed1b25f4c6420c874b6d3319
hossein7979 <hosseinfouladi20@gmail.com>
2021-05-24 19:16:33 +0430

    modified

--- Module/src/module-info.java
+++ Module/src/module-info.java
@@ -1,10 +1,8 @@
-/*module Module {
+module Module {
     requires javafx.base;
     requires javafx.controls;
-    requires javafx.fxml;
+   requires javafx.fxml;
     requires java.desktop;
-    requires jfxrt;
-    requires rt;
     opens codes.starter;
     opens codes.phases;
-}*/
+}
