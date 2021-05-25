ea9729b352cb99150b568daf57cd972c23d8ea64
hossein7979 <hosseinfouladi20@gmail.com>
2021-05-25 14:14:19 +0430

    filteredgraph created in grphGenerate class

--- Module/src/codes/picture/InputPicture.java
+++ Module/src/codes/picture/InputPicture.java
@@ -5,6 +5,15 @@ import java.io.File;
 public class InputPicture {
     File file;
 
+    //get fileaddress and create file:
+    public InputPicture(String fileAddress){
+        this.file=new File(fileAddress);
+    }
+
+    public InputPicture() {
+
+    }
+
     public Picture getFile() {
         Picture pic=new Picture(this.file);
         return pic;
